2b282889c3ecda88aea638990d6481b82d1d8583
okidog <gtracer465@gmail.com>
2024-09-17 11:03:15 -0400

    Error-check input, fix Collatz alg

--- asgt2/question1.c
+++ asgt2/question1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <pthread.h>
 
 int collatz(int n) {
     int rVal = 0;
 
     if (n == 1) rVal = 1;
-    else if (n % 2 == 1) rVal = n/2;
+    else if (n % 2 == 0) rVal = n/2;
     else rVal = (3*n)+1;
 
     return rVal;
@@ -12,11 +13,19 @@ int collatz(int n) {
 
 int main() {
     int n = 1;
-    printf("Enter a number:\n");
+    printf("Enter a number:\n"); // Scan user input
     scanf("%d", &n);
+
+    while (n <= 0) { // Error checking for non-positive integers
+        printf("Try again.\n");
+        scanf("%d", &n);
+    }
+
+    printf("%d ", n);
     do {
         n = collatz(n);
-        printf("%p ", &n);
+        printf("%d ", n);
     } while (n != 1);
+
     return 0;
 }
