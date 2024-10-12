3721dfbb49a3ca7b03240e5b77e6e38ae6781d23
okidog <gtracer465@gmail.com>
2024-10-11 23:18:54 -0400

    Working? 3.2

--- asgt3/question2/question2.c
+++ asgt3/question2/question2.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <time.h>
 
+# define BARISTA_COUNT 4
+
+pthread_t baristas[BARISTA_COUNT];
+pthread_mutex_t machines[BARISTA_COUNT];
 
 float generateTime() {
     float randTime = rand() % 1000;
@@ -12,22 +17,44 @@ float generateTime() {
     return randTime;
 }
 
-void leftMachine() {
-    sleep(generateTime());
-}
+void* barista(void* id) {
+    int baristaId = *((int *)id);
+    int leftMachine = baristaId;
+    int centerMachine = (baristaId + 1) % BARISTA_COUNT;
+    int rightMachine = (baristaId + 2) % BARISTA_COUNT;
 
-void centerMachine() {
-    sleep(generateTime());
-}
+    printf("Barista %d clocked in.\n", baristaId);
 
-void rightMachine() {
-    sleep(generateTime());
-}
+    while (1) {
+        printf("Barista %d is waiting for the coffee machines...\n", baristaId);
 
+        pthread_mutex_lock(&machines[leftMachine]);
+        pthread_mutex_lock(&machines[centerMachine]);
+        pthread_mutex_lock(&machines[rightMachine]);
+
+        printf("Barista %d is making coffee...\n", baristaId);
+        sleep(generateTime());
+
+        pthread_mutex_unlock(&machines[leftMachine]);
+        pthread_mutex_unlock(&machines[centerMachine]);
+        pthread_mutex_unlock(&machines[rightMachine]);
+        printf("Barista %d is done making coffee.\n", baristaId);
+    }
+}
 int main() {
     srand(time(NULL));
-    leftMachine();
-    centerMachine();
-    rightMachine();
+    int baristaId[BARISTA_COUNT];
+
+    for (int i = 0; i < BARISTA_COUNT; i++) {
+        pthread_mutex_init(&machines[i], NULL);
+        baristaId[i] = i;
+        pthread_create(&baristas[i], NULL, barista, &baristaId[i]);
+    }
+
+    for (int i = 0; i < BARISTA_COUNT; i++) {
+        pthread_join(baristas[i], NULL);
+        pthread_mutex_destroy(&machines[i]);
+    }
+
     return 0;
 }
